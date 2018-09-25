19bb352af87f754de0259f5367c8eb484c87c0a1
keshi <keshi.wang@earlydata.com>
2018-09-25 16:55:02 +0800

    提交代码

--- src/main/java/com/keshi/mytest/core/Testt.java
+++ src/main/java/com/keshi/mytest/core/Testt.java
@@ -5,6 +5,7 @@ public abstract class Testt {
 	public static void main(String[] args) {
 		System.out.println("2018年9月25日16:29:30 修改");
 		System.out.println("2018年9月25日16:42:26 修改");
+		System.out.println("2018年9月25日16:54:04 修改");
 	}
 	
 }
